c561e3ff8b99c7c51f4dd9985f7cd3c17d397ecc
Jan Pohanka <xhpohanka@gmail.com>
2020-06-16 13:09:36 +0200

    stm32h7: fix iwdg driver

--- soc/arm/st_stm32/stm32h7/soc.h
+++ soc/arm/st_stm32/stm32h7/soc.h
@@ -50,6 +50,10 @@
 #include <stm32h7xx_ll_wwdg.h>
 #endif
 
+#ifdef CONFIG_IWDG_STM32
+#include <stm32h7xx_ll_iwdg.h>
+#endif
+
 #ifdef CONFIG_SERIAL_HAS_DRIVER
 #include <stm32h7xx_ll_usart.h>
 #endif /* CONFIG_SERIAL_HAS_DRIVER */
